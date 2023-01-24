72b4855d086ec454ce0320f29da34e7c716984f4
tomonari <zcsxcacg@i.softbank.jp>
2023-01-24 15:29:16 +0900

    capybaraのセットアップ、テストで使うhtmlタグにidを付与

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'webdrivers'
 end
 
 group :development do
--- spec/system/calendars_spec.rb
+++ spec/system/calendars_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Calendars", type: :system do
   let(:user) { FactoryBot.create(:user) }
-  
+
   before do
-    sign_in :user
+    sign_in user
     visit root_path
   end
 
   scenario "user insert new schedule on day of calendar", js: true do
-
+    within "#day1" do
+      click_button
+      click_button "●"
+      expect(find('.calendar__day-body')).to have_content("●")
+    end
   end
 end
