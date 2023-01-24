70228fe7d5f364acaa3ed68ee420b163ec04b149
tomonari <zcsxcacg@i.softbank.jp>
2023-01-25 00:32:35 +0900

    システムスペックを３つ追加

--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -3,9 +3,15 @@
 FactoryBot.define do
   factory :setting do
     period_start_at { Date.new(2023, 1, 1) }
-    period_end_at { Date.new(2023, 12, 31) }
-    total_working_days { '180' }
-    schedule_of_sunday { 'full-time' }
+    period_end_at { Date.new(2023, 1, 31) }
+    total_working_days { '20' }
+    schedule_of_sunday { 'off' }
+    schedule_of_monday { 'full-time' }
+    schedule_of_tuesday { 'full-time' }
+    schedule_of_wednesday { 'full-time' }
+    schedule_of_thursday { 'full-time' }
+    schedule_of_friday { 'full-time' }
+    schedule_of_saturday { 'off' }
     association :calendar
   end
 end
