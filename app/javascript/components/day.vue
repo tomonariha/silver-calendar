ec3e718eece70cdb9b6553a8bfa55b7058dbbde3
tomonari <zcsxcacg@i.softbank.jp>
2023-03-15 18:06:04 +0900

    DayコンポーネントをCompositionAPIに変更

--- app/javascript/components/day.vue
+++ app/javascript/components/day.vue
@@ -9,86 +9,76 @@
   </Popper>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
 import Popper from 'vue3-popper'
-
-export default defineComponent({
-  data() {
-    return {
-      schedule: "",
-      scheduleMarks: ["●" , "▲" , "△" , "□", "指定なし"],
-      markToSchedule: { "●":"full-time", "▲":"morning", "△":"afternoon", "□":"off" , "指定なし":"" },
-      scheduleToMark: { "full-time":"●", "morning":"▲", "afternoon":"△", "off":"□" },
-    }
-  },
-  props: { 
-    date: { type: Object, required: true },
-    autoAdjusted: { type: Boolean, required: true },
-  },
-  methods: {
-    token() {
+const dayOfSchedule = ref("")
+const scheduleMarks = [ "●", "▲", "△", "□", "指定なし"]
+const markToSchedule = { "●":"full-time", "▲":"morning", "△":"afternoon", "□":"off" , "指定なし":"" }
+const scheduleToMark = { "full-time":"●", "morning":"▲", "afternoon":"△", "off":"□" }
+let props = defineProps({ 
+    date: Object,
+    autoAdjusted: Boolean})
+const emit = defineEmits(['update', 'delete'])
+function token() {
       const meta = document.querySelector('meta[name="csrf-token"]')
       return meta ? meta.getAttribute('content') : ''
-    },
-    currentSchedule() {
-      return this.scheduleToMark[this.date.schedule]
-    },
-    changeSchedule(scheduleMark) {
+    }
+function currentSchedule() {
+      return scheduleToMark[props.date.schedule]
+    }
+function changeSchedule(scheduleMark) {
       if (scheduleMark === "指定なし") {
-        this.deleteDate()
+        deleteDate()
       } else {
-        this.updateCalendar(scheduleMark)
+        updateCalendar(scheduleMark)
       }
-      this.schedule = this.markToSchedule[scheduleMark]
-    },
-    deleteDate() {
-      if (this.autoAdjusted) {
-        this.$emit('delete', this.date)
+      dayOfSchedule.value = markToSchedule[scheduleMark]
+    }
+function deleteDate() {
+  const date = props.date
+      if (props.date.autoAdjusted) {
+        emit('delete', date)
         return
       }
-      fetch(`days/${this.date.year}/${this.date.month}/${this.date.date}`, {
+      fetch(`days/${date.year}/${date.month}/${date.date}`, {
       method: 'DELETE',
       headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': this.token(),
+        'X-CSRF-Token': token(),
       },
       credentials: 'same-origin'
       })
       .then(() => {
-        this.$emit('delete', this.date)
+        emit('delete', date)
       })
       .catch((error) => {
         console.warn(error)
       })
-    },
-    updateCalendar(schedule) {
-      const dateState = {year: this.date.year, month: this.date.month, date: this.date.date, schedule: this.markToSchedule[schedule]}
-      if (this.autoAdjusted) {
-        this.$emit('update', dateState)
+    }
+function updateCalendar(schedule) {
+  const date = props.date
+      const dateState = {year: date.year, month: date.month, date: date.date, schedule: markToSchedule[schedule]}
+      if (props.date.autoAdjusted) {
+        emit('update', dateState)
         return
       }
-      fetch(`days/${this.date.year}/${this.date.month}`, {
+      fetch(`days/${date.year}/${date.month}`, {
       method: 'POST',
       headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': this.token(),
+        'X-CSRF-Token': token(),
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(dateState),
       credentials: 'same-origin'
       })
       .then(() => {
-        this.$emit('update', dateState)
+        emit('update', dateState)
       })
       .catch((error) => {
         console.warn(error)
       })
-    },
-  },
-  components: {
-    Popper,
-  },
-  emits: ['update', 'delete']
-})
+    }
+
 </script>
