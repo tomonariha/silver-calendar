527f4f4f13c9f6633f0eb1087e9d1a7857a0d0b0
tomonari <zcsxcacg@i.softbank.jp>
2023-04-03 22:37:32 +0900

    Deviseを使ったユーザーの編集・削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }                                        
   draw :api
   post 'days/:year/:month', to: 'days#update'
   delete 'days/:year/:month/:date', to: 'days#destroy'
