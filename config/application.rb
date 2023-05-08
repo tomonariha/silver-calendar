087301f05cb01dd4dad7ba5a6f3ca209a6083b3f
tomonari <zcsxcacg@i.softbank.jp>
2023-05-09 00:03:16 +0900

    Deviseのi18nをenに対応するようにした

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module WorkingDayDeployer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb, yml}')]
+    config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
