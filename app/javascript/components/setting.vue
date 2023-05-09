68288ec904859788bd8d5c17c229d9aec88355e5
tomonari <zcsxcacg@i.softbank.jp>
2023-05-10 03:17:58 +0900

    watchで監視して年が変わるたびにエラー表示がリセットされるようにした

--- app/javascript/components/setting.vue
+++ app/javascript/components/setting.vue
@@ -112,7 +112,7 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import Confirm from './confirm.vue'
 import { useToast } from "vue-toastification"
 
@@ -431,6 +431,12 @@ function totalDaysValidation(startDay, endDay) {
     return true
   }
 }
+watch(
+  () => props.year,
+  () => {
+    errors.value = []
+  }
+)
 </script>
 
 <style>
