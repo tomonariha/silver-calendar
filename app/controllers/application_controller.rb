34f4e1f867add080e77ae12c0df5d9dcb1b5b6bd
tomonari <zcsxcacg@i.softbank.jp>
2023-05-09 22:07:44 +0900

    Rubocop対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     calendar_path
   end
- 
+
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
