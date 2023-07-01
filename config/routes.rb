79e86554bd5e56bebbe1fe00c8d6ce12bb11548a
tomonari <zcsxcacg@i.softbank.jp>
2023-07-01 13:40:12 +0900

    ルーティングのresouces、namespaceを文字列からシンボルに変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'calendar', to: 'calendars#index', as: 'calendar'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  namespace 'api' do
-    namespace 'v1' do
-      resources 'calendars', only: %i(index destroy show update), param: :year do
-        resources 'settings', only: %i(index destroy create update)
-        resource 'alignment', only: %i(create destroy update)
+  namespace :api do
+    namespace :v1 do
+      resources :calendars, only: %i(index destroy show update), param: :year do
+        resources :settings, only: %i(index destroy create update)
+        resource :alignment, only: %i(create destroy update)
       end
       post 'days/:year/:month', to: 'days#update'
       delete 'days/:year/:month/:date', to: 'days#destroy'
-      resource 'users', only: %i(show)
-      resource 'times', only: %i(update)
+      resource :users, only: %i(show)
+      resource :times, only: %i(update)
     end
   end
   get 'privacy_policy', to: 'home#privacy_policy', as: 'privacy_policy'
