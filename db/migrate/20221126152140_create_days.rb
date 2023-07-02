f5dfa552e23d82c52dcab3fd8da33c3ff38a8a68
tomonari <zcsxcacg@i.softbank.jp>
2023-07-02 13:25:36 +0900

    マイグレーションファイルに空欄を追加

--- db/migrate/20221126152140_create_days.rb
+++ db/migrate/20221126152140_create_days.rb
@@ -1,8 +1,8 @@
 class CreateDays < ActiveRecord::Migration[7.0]
   def change
     create_table :days do |t|
-      t.references :calendar, foreign_key:true
-      t.date :date, null:false
+      t.references :calendar, foreign_key: true
+      t.date :date, null: false
       t.string :schedule
       t.boolean :locked 
       t.timestamps
