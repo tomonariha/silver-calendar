ec91ed3cdaf367036a5f9faeaba6eaee864dd78a
tomonari <zcsxcacg@i.softbank.jp>
2023-07-03 13:11:50 +0900

    api/calendars/alignments_controllerのviewで使わないインスタンス変数をローカル変数に変更

--- app/controllers/api/v1/calendars/alignments_controller.rb
+++ app/controllers/api/v1/calendars/alignments_controller.rb
@@ -4,45 +4,45 @@ module API
   module V1
     module Calendars
       class AlignmentsController < ApplicationController
-        before_action :set_user, :set_calendar, :set_calendar_client, :set_working_times
-      
         def create
           # resultにはGoogleカレンダーからのレスポンスが入る。
           # Googleカレンダーに新しく作ったカレンダーのIDを取得するための処理。
-          @result = @client.create_calendar(@calendar)
-          calendar_days = @calendar.days
-          @client.insert_events(calendar_days, @result.id, @working_times)
+          calendar = set_calendar
+          client = set_calendar_client
+          @result = client.create_calendar(calendar)
+          calendar_days = calendar.days
+          client.insert_events(calendar_days, @result.id, set_working_times)
         end
       
         def destroy
-          @result = @client.delete_calendar(@calendar)
+          calendar = set_calendar
+          client = set_calendar_client
+          @result = client.delete_calendar(calendar)
         end
       
         def update
           # Googleカレンダーに追加したカレンダーを削除し新しくカレンダーを作り直す
           # 個別に日程の予定を更新するにはevent_idが必要で処理が冗長になるため
-          @client.delete_calendar(@calendar)
-          @result = @client.create_calendar(@calendar)
-          calendar_days = @calendar.days
-          @client.insert_events(calendar_days, @result.id, @working_times)
+          calendar = set_calendar
+          client = set_calendar_client
+          client.delete_calendar(calendar)
+          @result = client.create_calendar(calendar)
+          calendar_days = calendar.days
+          client.insert_events(calendar_days, @result.id, set_working_times)
         end
       
         private
-      
-        def set_user
-          @user = current_user
-        end
-      
+
         def set_calendar
-          @calendar = current_user.calendars.find_by(year: params[:calendar_year])
+          current_user.calendars.find_by(year: params[:calendar_year])
         end
       
         def set_calendar_client
-          @client = CalendarClient.new(current_user)
+          CalendarClient.new(current_user)
         end
       
         def set_working_times
-          @working_times = params
+          params
         end
       end
     end
