4b5a566c25c34fc234fce6e017b88efdd7030429
tomonari <zcsxcacg@i.softbank.jp>
2023-07-07 17:32:40 +0900

    使わないDevise翻訳関係の設定やファイルを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery
-  around_action :switch_locale
   include CalendarsHelper
 
   def after_sign_in_path_for(_resource)
     calendar_path
-  end
-
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
   end
 end
