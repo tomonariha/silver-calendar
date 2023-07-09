9213406fc545757dc25c6f19a76a1f3345263636
tomonari <zcsxcacg@i.softbank.jp>
2023-07-09 12:19:19 +0900

    api/daysコントローラーのインスタンス変数をローカル変数に変更

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -3,27 +3,24 @@
 module API
   module V1
     class DaysController < ApplicationController
-      before_action :set_calendar
-      before_action :set_target_day
-    
       def destroy
-        day = @calendar.days.find_by(date: @target_day)
+        day = set_calendar.days.find_by(date: set_target_day)
         day&.delete
       end
     
       def update
-        day = @calendar.days.find_or_create_by(date: @target_day)
+        day = set_calendar.days.find_or_create_by(date: set_target_day)
         day.update!(schedule: params[:schedule])
       end
     
       private
     
       def set_calendar
-        @calendar = current_calendars.find_or_create_by!(year: params[:year])
+        current_calendars.find_or_create_by!(year: params[:year])
       end
     
       def set_target_day
-        @target_day = Date.new(params[:year].to_i, params[:month].to_i, params[:date].to_i)
+        Date.new(params[:year].to_i, params[:month].to_i, params[:date].to_i)
       end
     end
   end
