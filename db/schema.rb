b7e18b5ff3030a8e99c887a768501fe238d178aa
tomonari <zcsxcacg@i.softbank.jp>
2023-07-12 16:08:56 +0900

    使わないusernameのカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_053240) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_070722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_053240) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "username"
     t.integer "morning_start_hour", default: 8
     t.integer "morning_start_minit", default: 0
     t.integer "morning_end_hour", default: 12
