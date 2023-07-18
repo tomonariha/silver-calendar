7d932fbf83b501f0102839e83bd99a017f7fb762
tomonari <zcsxcacg@i.softbank.jp>
2023-07-18 14:34:51 +0900

    マイグレーションファイルをリバーシブルにした

--- db/migrate/20230718020211_change_default_schedule_to_days.rb
+++ db/migrate/20230718020211_change_default_schedule_to_days.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class ChangeDefaultScheduleToDays < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :days, :schedule, :string, default: 'None'
+  end
+
+  def down
+    change_column :days, :schedule, :string
   end
 end
